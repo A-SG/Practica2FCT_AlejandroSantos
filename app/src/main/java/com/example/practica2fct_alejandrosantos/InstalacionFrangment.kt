eff8f22f3f8abe94a82e0fbf2af549df846a51d6
Alejandro Santos Gutierrez <alejandrosantosgut@gmail.com>
2023-05-08 09:36:12 +0200

    Configuración de fragmento denominado "Mi Actividad", añadiendo los recursos en el archivo Strings necesarios y configuracion de binding en la Activity del fragmento

---
+++ app/src/main/java/com/example/practica2fct_alejandrosantos/InstalacionFrangment.kt
@@ -0,0 +1,38 @@
+package com.example.practica2fct_alejandrosantos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.practica2fct_alejandrosantos.databinding.FragmentInstalacionFrangmentBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [InstalacionFrangment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class InstalacionFrangment : Fragment() {
+
+    private lateinit var  binding: FragmentInstalacionFrangmentBinding
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentInstalacionFrangmentBinding.inflate(layoutInflater)
+        return binding.root
+
+    }
+}
---
+++ app/src/main/java/com/example/practica2fct_alejandrosantos/data/network/domain/ui/view/PantallaPrincipalSmartSolar.kt
@@ -0,0 +1,12 @@
+package com.example.practica2fct_alejandrosantos.data.network.domain.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.practica2fct_alejandrosantos.R
+
+class PantallaPrincipalSmartSolar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_principal_smart_solar)
+    }
+}
