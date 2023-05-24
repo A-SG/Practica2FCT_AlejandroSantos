52d26ccdf464072f67cbc2cde772dfd4ba962dd4
Alejandro Santos Gutierrez <alejandrosantosgut@gmail.com>
2023-05-24 11:03:14 +0200

    Configuración de la vista de Login

--- app/src/main/java/com/example/practica2fct_alejandrosantos/data/network/domain/ui/view/Login.kt
+++ app/src/main/java/com/example/practica2fct_alejandrosantos/data/network/domain/ui/view/Login.kt
@@ -3,10 +3,16 @@ package com.example.practica2fct_alejandrosantos.data.network.domain.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.practica2fct_alejandrosantos.R
+import com.example.practica2fct_alejandrosantos.databinding.ActivityLoginBinding
+import com.example.practica2fct_alejandrosantos.databinding.ActivityMainBinding
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
